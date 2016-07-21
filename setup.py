8a9fa09549159ee981528e5a68f74f2572cc10e7
Aleksey Zhukov <drdaeman@drdaeman.pp.ru>
2016-07-21 22:29:24 +0300

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="le-client",
-    version="0.1",
+    version="0.2",
     packages=["le_client"],
     include_package_data=True,
     license="GNU GPLv3+",
